a77fe2a3d90c998ba3f168fcbd27ea9a66a9b0c8
Minh An Dao <minhan7497@gmail.com>
2019-07-15 13:53:06 +0700

    ADC adjusting

--- Standalone_4-20mA_ADC/Standalone_4-20mA_ADC.ino
+++ Standalone_4-20mA_ADC/Standalone_4-20mA_ADC.ino
@@ -7,10 +7,11 @@
   ADC_11db: sets an attenuation of 3.6 (1V input = ADC reading of 3959).
 */
 #include <driver/adc.h>
-
+#include <SimpleKalmanFilter.h>
+SimpleKalmanFilter filter(2, 2, 0.01);
+float es_senVal=0;
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  Serial.begin(115200);// initialize serial communication at 115200 bits per second
   analogReadResolution(11); // Default of 12 is not very linear. Recommended to use 10 or 11 depending on needed resolution.
   analogSetWidth(11); //Range 0-2047
   analogSetAttenuation(ADC_6db); // Default is 11db which is very noisy. Recommended to use 2.5 or 6.
@@ -19,9 +20,13 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog ADC1_0:
-  int sensorValue = analogRead(A0);
+  // read the input on analog ADC1_0 (VP pin):
+  int senVal = analogRead(34);
+  es_senVal = filter.updateEstimate(senVal); // 1 layer
+  for (int a=0;a<4; a++) {
+    es_senVal = filter.updateEstimate(es_senVal); // 2 layer  
+  }//end for
   // print out the value you read:
-  Serial.println(sensorValue);
+  Serial.println(es_senVal);
   delay(100);        // delay in between reads for stability
 }
